ff2664498b99bb1c9aee9f5a6ec94ac4dddd9514
anubhavhajela4 <anubhavhajela4@gmail.com>
2025-09-13 12:55:29 +0530

    Added Websockets for ChatApp

---
+++ src/main/java/com/substring/chat/payload/MessageRequest.java
@@ -0,0 +1,18 @@
+package com.substring.chat.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageRequest {
+    private String sender;
+    private String content;
+    private String roomId;
+}
